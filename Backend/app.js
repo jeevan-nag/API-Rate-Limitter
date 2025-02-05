7764bfb4fa97ebbb75f31a4174030307d6f1a934
jeevan_n <jeevan.n@portqii.com>
2025-02-05 16:07:01 +0530

    #Initial Commit

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process?.env?.PORT || 8080;
+
+app.get("/test", (req, res) => {
+    res.status(200).send({ message: `API Rate Limitter Backend is running @ ${PORT}` });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
